7e569280655e99b47a2b0a4cc138308d600a034a
takumi314 <isospin_symmetry@me.com>
2016-09-30 02:35:07 +0900

    Create APIViewController, Set inital ViewController

---
+++ bitRate/APIViewController.swift
@@ -0,0 +1,13 @@
+//
+//  APIViewController.swift
+//  bitRate
+//
+//  Created by NishiokaKohei on 2016/09/30.
+//  Copyright © 2016年 NishiokaKohei. All rights reserved.
+//
+
+import UIKit
+
+class APIViewController: UIViewController {
+
+}
