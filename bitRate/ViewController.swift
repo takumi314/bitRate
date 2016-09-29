4e8ab4122a18942c7c2253e47ca5d83ebd43f0dd
takumi314 <isospin_symmetry@me.com>
2016-09-30 02:03:21 +0900

    Init

---
+++ bitRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bitRate
+//
+//  Created by NishiokaKohei on 2016/09/30.
+//  Copyright © 2016年 NishiokaKohei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
