c723f1dfeea6c9d3afdc218393a14d4a35458795
takumi314 <isospin_symmetry@me.com>
2016-10-06 00:15:22 +0900

    Add-ViewController# Add TableViewDelegete

--- bitRate/APIViewController.swift
+++ bitRate/APIViewController.swift
@@ -12,25 +12,63 @@ import SwiftyJSON
 
 class APIViewController: UIViewController {
     
-    let table = UITableView()
+    @IBOutlet var table:UITableView!
+    var dataPrices = [[String:AnyObject]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "JPY/BTC"
         
-        table.frame = view.frame
-        view.addSubview(table)
-        
         getAPI()
+        
     }
     
     func getAPI() {
         Alamofire.request(.GET, "https://api.zaif.jp/api/1/last_price/btc_jpy")
-            .responseJSON { response in
-                print(response.result.value)
+            .responseJSON { (responseData) -> Void in
+             
+                if responseData.result.value != nil {
+                    let swiftyJasonVar = JSON(responseData.result.value!)
+                    self.getData(swiftyJasonVar)
+                }
+                
+            }
+    }
+
+    func getData(swifyJSON:JSON) {
+        if let data = swifyJSON["last_price"].arrayObject {
+            self.dataPrices = data as! [[String:AnyObject]]
+        }
+
+        refreshTable()
+    }
+    
+    func refreshTable() {
+        if self.dataPrices.count > 0 {
+            self.table.reloadData()
         }
     }
     
     
     
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("jsonCell")!
+        var currncies = dataPrices[indexPath.row]
+        
+        cell.textLabel?.text = currncies["last_price"] as? String
+        cell.detailTextLabel?.text = currncies["email"] as? String
+        
+        return cell
+    
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataPrices.count
+    }
+        
+
 }
+
+
+
